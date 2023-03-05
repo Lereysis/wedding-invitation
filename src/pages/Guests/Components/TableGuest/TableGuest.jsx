021fa01f41b6d36a2e0b76f86e5b148c58362acf
Luis Martinez <mr.luiswebmaster@gmail.com>
2023-03-04 19:38:18 -0500

    testing link

--- src/pages/Guests/Components/TableGuest/TableGuest.jsx
+++ src/pages/Guests/Components/TableGuest/TableGuest.jsx
@@ -90,7 +90,7 @@ const TableGuest = () => {
         try {
             const response = await api.post('/send-message-reminder', {
                 number,
-                url: `${window.location.origin}/${id}/${name.replaceAll(' ','%')}/formulario-de-recordatorio`,
+                url: `${window.location.origin}/${id}/${name.replaceAll(' ','%').trim()}/formulario-de-recordatorio`,
             })
             MySwal.close()
             
