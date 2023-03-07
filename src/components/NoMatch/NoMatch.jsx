7a11ae24f24e6328957f0549e5e6224caf8ee546
Lereysis <lereysisquezada@gmail.com>
2023-03-06 22:07:08 -0500

    page not found

--- src/components/NoMatch/NoMatch.jsx
+++ src/components/NoMatch/NoMatch.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import './NoMatch.scss'
+import { Link } from 'react-router-dom';
+
 
 const NoMatch = () => {
   return (
-    <div>NoMatch</div>
+    <div className='container-not-found'>
+      <h1>¡Ups al parecer hubo un error!</h1>
+      <h1 style={{fontSize:'10rem'}} className='text-secondary fw-bold' >404</h1>
+      <p>Pulsa en el botón para volver</p>
+      <Link to="/administracion-invitados"><button className='btn btn-primary' style={{fontWeight:'600'}} >Inicio</button></Link>
+    </div>
   )
 }
 
